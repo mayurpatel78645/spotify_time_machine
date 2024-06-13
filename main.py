9e911a077865134283b431b53723e3c1a6071614
mayur <mayurpatel78645@gmail.com>
2024-06-13 15:46:09 -0500

    add top 100 billboard songs to hollywood.txt

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 
 bollywood_url = "https://www.bbc.co.uk/asiannetwork/vote/top-songs/"
+hollywood_url = "https://www.billboard.com/charts/hot-100/2016-07-02/"
+urls = [bollywood_url, hollywood_url]
 
-request = requests.get(bollywood_url)
-response = request.text
-soup = BeautifulSoup(response, "html.parser")
 
-bollywood_titles = [title.getText() for title in soup.select(".intro") if "-" in title.getText()][1:]
+def scrape(url):
+    request = requests.get(url)
+    response = request.text
+    soup = BeautifulSoup(response, "html.parser")
+    if "billboard" in url:
+        titles = [song.getText().strip() for song in soup.select("h3") if ":" not in song.getText()][3:103]
+    else:
+        titles = [song.getText() for song in soup.select(".intro") if "-" in song.getText()][1:]
+    return titles
 
-with open("bollywood.txt", "w") as file:
-    for title in bollywood_titles:
-        file.write(f"{title}\n")
+
+def create_txt_file(file_name, url):
+    bollywood_titles = scrape(url)
+    with open(f"{file_name}.txt", "w") as file:
+        for title in bollywood_titles:
+            file.write(f"{title}\n")
+
+
+for url in urls:
+    create_txt_file("hollywood" if "billboard" in url else "bollywood", url)
